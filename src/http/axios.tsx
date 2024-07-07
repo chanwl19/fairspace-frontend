17310f0c45776bf4d3fc9f17270f5290ca975ca3
chanwl1988 <chanwl1988@gmail.com>
2024-07-07 20:29:34 +0000

    change

--- src/http/axios.tsx
+++ src/http/axios.tsx
@@ -3,5 +3,6 @@ const BASE_URL = 'https://fairspace-backend-ts.vercel.app/';
 
 export default axios.create({
     baseURL: BASE_URL,
-    headers: { 'Content-Type': 'application/json' }
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true
 });
