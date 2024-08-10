939d2e22c75e02d779b96e08fd263b0f8d644f65
chanwl1988 <chanwl1988@gmail.com>
2024-08-10 00:29:29 +0000

    fix date time issue

--- src/components/reservation/components/ReservationCard.tsx
+++ src/components/reservation/components/ReservationCard.tsx
@@ -40,7 +40,7 @@ export default function ReservationCard({ reservation, cancelReservationHandler,
       loadingHanlder(false);
     }
   }
-
+  
   return (
     <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
       <div className="mt-4 flex items-end justify-between">
--- src/utils/dateFormat.tsx
+++ src/utils/dateFormat.tsx
@@ -2,5 +2,5 @@ const days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
 
 export function convertDateToString(inputDate: string): string{
     const date  = new Date(inputDate);
-    return days[date.getUTCDay()] + ' ' + date.getUTCDate().toString() + '-'+ (date.getUTCMonth() +1).toString()+'-'+date.getUTCFullYear().toString() + ' ' + date.getUTCHours().toString()+':'+date.getUTCMinutes().toString();
+    return days[date.getUTCDay()] + ' ' + date.getUTCDate().toString() + '-'+ (date.getUTCMonth() +1).toString()+'-'+date.getUTCFullYear().toString() + ' ' + (date.getUTCHours().toString()).padStart(2, '0')+':'+(date.getUTCMinutes().toString()).padStart(2,'0');
 };
